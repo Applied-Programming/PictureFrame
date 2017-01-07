f73eff5d81c9fe22d27a3e97affc7318c494f59e
Applied-Programming <aniruddhastapas@gmail.com>
2017-01-08 03:59:45 +0530

    Commit

---
+++ PictureBox/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd_obj = new OpenFileDialog();
+            if (ofd_obj.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            { 
+                //Select Image
+               Image img = Image.FromFile(ofd_obj.FileName);
+                    pictureBox1.Image= img;
+            }
+        }
+    }
+}
